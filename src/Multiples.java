570520911818f2a29d6c7ca8b5c67efb5c468d4a
Katia719 <ebrechko2022@gmail.com>
2023-09-11 14:53:46 -0400

    created file and implemented function

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Multiples {
+public class Multiples {
+    public static void main(String[] args) {
+        int multSoFar = 0;
+        for (int curr = 999; curr > 0; curr--){
+            boolean divisibleBy3 = curr % 3 == 0;
+            boolean divisibleBy5 = curr % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                multSoFar++;
+            }
+        }
+        System.out.println(multSoFar);
+    }
 }
