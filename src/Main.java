bb16e4464c3828a02612b0b43d09f0ce4d6bba23
Katia719 <ebrechko2022@gmail.com>
2023-09-11 14:14:44 -0400

    adding the entire Main file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        // to fix FizzBuzz.main();, we passed args
+        FizzBuzz.main(args);
+    }
 }
