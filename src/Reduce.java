8cd9f979aeb575ca10371575e1e79156ceed8d41
Katia719 <ebrechko2022@gmail.com>
2023-09-11 14:52:50 -0400

    created file Reduce and implemented function

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int numSteps = 0;
+        int curr = 100;
+        while (curr > 0) {
+            if (curr % 2 == 0) {
+                curr = curr / 2;
+            }
+            else {
+                curr = curr - 1;
+            }
+            numSteps++;
+        }
+        System.out.println(numSteps);
+    }
 }
